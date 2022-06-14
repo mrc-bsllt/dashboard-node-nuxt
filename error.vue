bbb05ec9b1aece905e9ed7e234e6c61697441988
Marco Busellato <marco.busellato@develondigital.com>
2022-06-14 12:12:38 +0200

    added statusCode dynamically

--- error.vue
+++ error.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
 main#error-page(class="w-full h-screen flex flex-col justify-center items-center flex-nowrap bg-black overflow-hidden")
-  h1(class="uppercase text-60 text-grey font-semibold") 404 error
+  h1(class="uppercase text-60 text-grey font-semibold") {{ props.error?.statusCode }} error
   button(@click="handleError" class="text-grey underline") Come back!
 </template>
 
-<script setup>
+<script setup lang="ts">
 const props = defineProps({
   error: Object
 })
