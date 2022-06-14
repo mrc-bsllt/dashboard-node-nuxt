b01de039cb677dd19c847c88414ac2298ec75932
Marco Busellato <marco.busellato@develondigital.com>
2022-06-14 12:09:04 +0200

    add 404 page

---
+++ error.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+main#error-page(class="w-full h-screen flex flex-col justify-center items-center flex-nowrap bg-black overflow-hidden")
+  h1(class="uppercase text-60 text-grey font-semibold") 404 error
+  button(@click="handleError" class="text-grey underline") Come back!
+</template>
+
+<script setup>
+const props = defineProps({
+  error: Object
+})
+
+const handleError = () => clearError({ redirect: '/' })
+</script>
+
