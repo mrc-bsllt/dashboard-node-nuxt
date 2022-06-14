712b038c5e4a7ccfdda7437f9e8467db200619a9
Marco Busellato <marco.busellato@develondigital.com>
2022-06-14 14:09:29 +0200

    add home icon to the header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-header#header(class="py-3 px-6 bg-black text-grey border-b border-solid border-gold")
+header#header(class="relative py-4 px-6 bg-black text-grey border-b border-solid border-gold")
+  nuxt-link(to="/" class="absolute top-1/2 left-[24px] -translate-y-1/2")
+    icon(name="home")
   ul(class="header__list w-full h-full flex flex-row justify-end items-center flex-nowrap") 
     template(v-if="!get_show_logout")
       li.px-2
@@ -15,6 +17,11 @@ header#header(class="py-3 px-6 bg-black text-grey border-b border-solid border-g
 
 <script setup lang="ts">
 import { useHeader } from '@/store/header'
+import Icon from '@/components/commons/Icon.vue'
+defineNuxtComponent({
+  Icon
+})
+
 const headerStore = useHeader()
 const { get_show_logout } = toRefs(headerStore)
 
---
+++ components/commons/Icon.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+svg.ico(:class="`icon-${name}`" v-bind="$attrs")
+  use(:xlink:href="`/ico-defs.svg?v=20210108#icon-${name}`")
+</template>
+
+<script setup lang="ts">
+defineProps({
+  name: { type: String, required: true }
+})
+</script>
